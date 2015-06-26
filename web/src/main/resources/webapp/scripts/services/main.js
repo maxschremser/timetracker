58c08c129e292f6fde2c1c310f681bb9555f6681
bluemax <maxi@schremser.com>
2015-06-26 06:55:22 +0200

    start using restangular

---
+++ web/src/main/resources/webapp/scripts/services/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name Date REST API Implementation
+ * @description
+ * # adminPosHeader
+ */
+angular.module('timetracker', ['restangular'])
+    .factory('DateApi', function(rest){
+        console.log("DateApi call")
+    });
+
+
